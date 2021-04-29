f9beb88297324ac57a686c046fc544318a580a57
Yslan Ramos <yslan3000@gmail.com>
2021-04-29 00:52:34 -0300

    test: convert Embed to map

--- test/utils/embed_validator_test.exs
+++ test/utils/embed_validator_test.exs
@@ -1,11 +1,17 @@
 defmodule ElixirTest.Utils.EmbedValidatorTest do
   use ExUnit.Case
   alias Alchemy.Embed
+  require Alchemy.Embed
   alias MyElixir.Utils.EmbedValidator
   doctest MyElixir.Utils.EmbedValidator
 
-  test "should return the embed when is empty" do
+  defp build_embed do
     embed = %Embed{}
+    Map.from_struct(embed)
+  end
+
+  test "should return the embed when is empty" do
+    embed = build_embed()
     assert EmbedValidator.valid?(embed) == embed
   end
 end
