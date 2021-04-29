200a9c03b445eccdb70ab98b6f7f53da5a4f0c8c
Yslan Ramos <yslan3000@gmail.com>
2021-04-29 01:43:31 -0300

    feat: add testing for embed title

--- lib/utils/embed_validator.ex
+++ lib/utils/embed_validator.ex
@@ -1,7 +1,16 @@
 defmodule MyElixir.Utils.EmbedValidator do
+  alias Alchemy.Embed
   alias Skooma.Validators
 
   def valid? (embed) do
+    if is_struct(embed) do
+      do_valid?(Map.from_struct(embed))
+    else
+      do_valid?(embed)
+    end
+  end
+
+  defp do_valid? (embed) do
     case Skooma.valid?(embed, valid_embed_schema()) do
       {:error, reason} -> {:error, reason}
       :ok -> embed
--- test/utils/embed_validator_test.exs
+++ test/utils/embed_validator_test.exs
@@ -5,13 +5,37 @@ defmodule ElixirTest.Utils.EmbedValidatorTest do
   alias MyElixir.Utils.EmbedValidator
   doctest MyElixir.Utils.EmbedValidator
 
-  defp build_embed do
-    embed = %Embed{}
-    Map.from_struct(embed)
+  defp embed_equal?(source, target) do
+    Map.equal?(to_map(source), to_map(target))
   end
 
+  defp to_map (struct) do
+    if is_struct(struct) do
+      Map.from_struct(struct)
+    else
+      struct
+    end
+  end
+
+
   test "should return the embed when is empty" do
-    embed = build_embed()
-    assert EmbedValidator.valid?(embed) == embed
+    embed = %Embed{}
+    assert embed_equal?(EmbedValidator.valid?(embed), embed)
+  end
+
+  test "should return the embed with title" do
+    title_embed = %Embed{ title: "valid_title" }
+    assert embed_equal?(EmbedValidator.valid?(title_embed), title_embed)
+  end
+
+  test "should trigger an error when title is not string" do
+    {:error, _reason} = EmbedValidator.valid?(%Embed{ title: 1 })
+    {:error, _reason} = EmbedValidator.valid?(%Embed{ title: :test })
+    {:error, _reason} = EmbedValidator.valid?(%Embed{ title: %{} })
+  end
+
+  test "should trigger an error when title is must great" do
+    big_title = String.duplicate("invalid_title", 400)
+    {:error, _reason} = EmbedValidator.valid?(%Embed{ title: big_title })
   end
 end
